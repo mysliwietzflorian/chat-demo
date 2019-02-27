c685114b50b2d86b77158da3180a72b6cff1a0ff
Mysliwietz Florian <myslflo1996@yahoo.de>
2019-02-27 01:31:30 +0100

    Implement basic post append on chat

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -3,3 +3,40 @@ console.log('Hello, world.');
 function sendMessage() {
 	console.info('Implement send-message');
 }
+
+function addPost(name, message) {
+	let chat = document.getElementById('chat');
+
+	let post = document.createElement('div');
+	post.className = "chat__post d-flex";
+
+	let img = document.createElement('img');
+	img.className = 'chat__avatar';
+	img.src = 'http://placehold.jp/50x50.png';
+	post.appendChild(img);
+
+	let chatBody = document.createElement('div');
+	chatBody.className = 'chat__body';
+	post.appendChild(chatBody);
+
+	let row = document.createElement('div');
+	row.className = 'd-flex';
+	chatBody.appendChild(row);
+
+	let chatUsername = document.createElement('div');
+	chatUsername.className = 'chat__username';
+	chatUsername.innerHTML = name || 'Username';
+	row.appendChild(chatUsername);
+
+	let chatTimestamp = document.createElement('div');
+	chatTimestamp.className = 'chat__timestamp';
+	chatTimestamp.innerHTML = '08:49 AM';
+	row.appendChild(chatTimestamp);
+
+	let chatMessage = document.createElement('div');
+	chatMessage.className = 'chat__message';
+	chatMessage.innerHTML = message || 'This is a short message.';
+	chatBody.appendChild(chatMessage);
+
+	chat.appendChild(post);
+}
