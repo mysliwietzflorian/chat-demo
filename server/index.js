927f1331f448e58eb3b0f5bbe679408c9cd47a13
Mysliwietz Florian <myslflo1996@yahoo.de>
2019-02-28 01:50:19 +0100

    Add user connection notification logic on server

--- server/index.js
+++ server/index.js
@@ -12,9 +12,24 @@ server.listen(PORT, () => {
 app.use(express.static(`${__dirname}/../client/public`));
 
 io.on('connection', socket => {
+	socket.data = [];
+	socket.data['isAuthenticated'] = false;
+
 	console.log(`[INFO]  User connected`);
 
 	socket.on('disconnect', () => {
 		console.log(`[INFO]  User disconnected`);
+	});
+
+	socket.on('user-connect', username => {
+		if (socket.data['isAuthenticated']) {
+			return;
+		}
+
+		socket.data['isAuthenticated'] = true;
+		socket.data['username'] = username;
+		console.log(`[INFO]  User authenticated with ${username}`);
+
+		socket.broadcast.emit('user-connect', username);
 	});
 });
